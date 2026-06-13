7dc5fc90f37fc354b3aec160a8ea888c7311afa1
mortadha20 <mortadhaboufeden17@gmail.com>
2026-06-13 15:26:54 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import Portfolio from "./portfolio";
 
 function App() {
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-DVCK86VKK0");
+    }
+  }, []);
+
   return <Portfolio />;
 }
 
